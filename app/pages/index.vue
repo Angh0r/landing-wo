b104dc235761774202686cca89b88ffd5b27cc1d
Konrad Deskiewicz <konrad.deskiewicz@hexant.eu>
2025-12-11 18:11:00 +0100

    Chore: id's added for navigation

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <AboutUsSection />
-    <SelectionSection />
-    <CtaSection />
-    <FaqSection />
+    <AboutUsSection id="about-us" />
+    <SelectionSection id="how-it-works" />
+    <CtaSection id="cta" />
+    <FaqSection id="faq" />
   </div>
 </template>
