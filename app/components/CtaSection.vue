82c577e6b1be1653623ff74af1b5fe9193458b5b
Konrad Deskiewicz <konrad.deskiewicz@hexant.eu>
2025-12-11 15:37:29 +0100

    Feat: CTA Section (WIP)

---
+++ app/components/CtaSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="px-4 lg:px-16 py-32 flex flex-col items-center gap-8">
+    <div
+      class="flex flex-col items-center text-6xl font-black text-black uppercase tracking-tighter"
+    >
+      <p class="text-center">
+        {{ $t('cta.title1') }}<br />
+        {{ $t('cta.title2') }}
+      </p>
+      <p class="text-center">{{ $t('cta.title3') }}</p>
+    </div>
+    <p class="text-xl text-center">
+      {{ $t('cta.description1') }}<br />
+      {{ $t('cta.description2') }}
+    </p>
+    <TesterButton />
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="postcss" scoped></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <CtaSection />
     <FaqSection />
   </div>
 </template>
