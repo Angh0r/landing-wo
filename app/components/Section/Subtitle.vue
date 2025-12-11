461ccdcbe5d2443269f5a123d2993ca01b396ca9
Konrad Deskiewicz <konrad.deskiewicz@hexant.eu>
2025-12-11 16:21:41 +0100

    Feat: Section title and subtitle components

---
+++ app/components/Section/Subtitle.vue
@@ -0,0 +1,9 @@
+<template>
+  <h3 class="text-xl text-center py-2 max-w-4xl mx-auto">
+    <slot></slot>
+  </h3>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="postcss" scoped></style>
---
+++ app/components/Section/Title.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2
+    class="text-4xl font-semibold text-black tracking-tighter text-center py-2 max-w-4xl mx-auto"
+  >
+    <slot></slot>
+  </h2>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="postcss" scoped></style>
