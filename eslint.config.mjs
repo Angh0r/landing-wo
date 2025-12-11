c8b7a6dd40e74e8a9b498ad4e22eb68ae7e73b9e
Konrad Deskiewicz <konrad.deskiewicz@hexant.eu>
2025-12-11 15:21:58 +0100

    Chore: Eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'vue/no-multiple-template-root': 'off',
+      'vue/html-self-closing': 'off',
+    },
+  },
+])
