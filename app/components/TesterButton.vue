9309481cca0480be01456971cfb8e54bede46cd6
Konrad Deskiewicz <konrad.deskiewicz@hexant.eu>
2025-12-12 12:25:45 +0100

    Chore: NuxtUI UButton rather than styled link

--- app/components/TesterButton.vue
+++ app/components/TesterButton.vue
@@ -1,10 +1,11 @@
 <template>
-  <NuxtLink
+  <UButton
+    size="xl"
+    class="bg-brand-orange hover:bg-brand-orange text-white px-6 py-3 rounded-full hover:scale-95 transition-all duration-500"
     :to="$localePath({ name: 'contact' })"
-    class="bg-brand-orange text-white px-6 py-3 rounded-full hover:scale-95 transition-all duration-500"
   >
     {{ $t('become_a_tester') }}
-  </NuxtLink>
+  </UButton>
 </template>
 
 <script setup lang="ts"></script>
