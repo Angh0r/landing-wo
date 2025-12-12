785f6a55aaede436de313d2a977b622efecbe848
Konrad Deskiewicz <konrad.deskiewicz@hexant.eu>
2025-12-12 12:25:09 +0100

    chore: main tag added

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <AppHeader />
-    <slot />
+    <main>
+      <NuxtPage />
+    </main>
     <AppFooter />
   </div>
 </template>
- 
+
 <script setup lang="ts"></script>
- 
+
 <style lang="postcss" scoped></style>
