76e8c4dbfc404676fabe2a799c700b93fa19abf4
Konrad Deskiewicz <konrad.deskiewicz@hexant.eu>
2025-12-12 14:23:11 +0100

    feat: slide animation added to cta

--- app/components/CtaSection.vue
+++ app/components/CtaSection.vue
@@ -3,11 +3,13 @@
     <div
       class="flex flex-col items-center text-6xl font-black text-black uppercase tracking-tighter"
     >
-      <p class="text-center">
+      <p class="text-center animation-slide slide-right">
         {{ $t('cta.title1') }}<br />
         {{ $t('cta.title2') }}
       </p>
-      <p class="text-center">{{ $t('cta.title3') }}</p>
+      <p class="text-center animation-slide slide-left">
+        {{ $t('cta.title3') }}
+      </p>
     </div>
     <p class="text-xl text-center">
       {{ $t('cta.description1') }}<br />
@@ -19,4 +21,34 @@
 
 <script setup lang="ts"></script>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+@media (prefers-reduced-motion: no-preference) {
+  .slide-right {
+    animation: slide-right 0.5s ease-in-out;
+    animation-timeline: view(40vh 0);
+  }
+
+  .slide-left {
+    animation: slide-left 0.5s ease-in-out;
+    animation-timeline: view(40vh 0);
+  }
+}
+
+@keyframes slide-right {
+  from {
+    transform: translateX(-30%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slide-left {
+  from {
+    transform: translateX(30%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+</style>
