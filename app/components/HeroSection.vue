732b6ce4ed4c08fdb14fd1fd91905ce1b07a785e
Konrad Deskiewicz <konrad.deskiewicz@hexant.eu>
2025-12-15 16:52:04 +0100

    Feat: Hero animations

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -8,17 +8,19 @@
         <div class="lg:w-3/5 grid gap-16">
           <div class="grid gap-8">
             <div
-              class="text-black uppercase font-black text-4xl md:text-[clamp(2.5rem,calc(1rem+6vw),5rem)] lg:text-[clamp(4rem,calc(1rem+5vw),5rem)] leading-none flex flex-col items-center lg:items-start"
+              class="text-black uppercase font-black text-4xl md:text-[clamp(2.5rem,calc(1rem+6vw),5rem)] lg:text-[clamp(4rem,calc(1rem+5vw),5rem)] leading-none flex flex-col items-center lg:items-start animation-show show-500 opacity-0"
             >
               <p>{{ $t('hero.title.line1') }}</p>
               <p>{{ $t('hero.title.line2') }}</p>
               <p>{{ $t('hero.title.line3') }}</p>
             </div>
-            <p class="text-2xl text-zinc-600">
+            <p class="text-2xl text-zinc-600 animation-show show-700 opacity-0">
               {{ $t('hero.subtitle') }}
             </p>
           </div>
-          <div class="flex justify-center lg:justify-start">
+          <div
+            class="flex justify-center lg:justify-start animation-show show-1000 opacity-0"
+          >
             <TesterButton />
           </div>
         </div>
@@ -27,7 +29,7 @@
           src="/img/mockup_macbook_magazyny.webp"
           width="1008"
           height="635"
-          class="lg:object-cover h-auto lg:h-[70vh] w-full lg:overflow-visible lg:object-left"
+          class="lg:object-cover h-auto lg:h-[70vh] w-full lg:overflow-visible lg:object-left animation-reveal-to-left show-500 opacity-0"
         />
       </div>
 
@@ -42,4 +44,42 @@
 
 <script setup lang="ts"></script>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.animation-reveal-to-left {
+  animation: reveal-to-left 2s ease-in-out forwards;
+}
+
+.animation-show {
+  opacity: 0;
+  animation: show 2s ease-in-out forwards;
+}
+
+.show-500 {
+  animation-delay: 0.5s;
+}
+
+.show-700 {
+  animation-delay: 0.7s;
+}
+
+.show-1000 {
+  animation-delay: 1s;
+}
+
+@keyframes show {
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes reveal-to-left {
+  from {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+</style>
